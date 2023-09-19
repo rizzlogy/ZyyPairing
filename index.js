6f5a881957e99264061800e11bf4eb1e3ce98a3e
rizzlogy <rizzlogy@users.noreply.github.com>
2023-09-19 20:51:31 +0000

    automatic tidy up code

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
 const pino = require("pino");
-const path = require('path');
-const colors = require('@colors/colors/safe')
-const CFonts = require('cfonts')
+const path = require("path");
+const colors = require("@colors/colors/safe");
+const CFonts = require("cfonts");
 const fs = require("fs-extra");
 const chalk = require("chalk");
-const readline = require("readline")
-const { 
-default: makeWASocket, 
-useMultiFileAuthState, 
-delay,
-PHONENUMBER_MCC 
+const readline = require("readline");
+const {
+  default: makeWASocket,
+  useMultiFileAuthState,
+  delay,
+  PHONENUMBER_MCC,
 } = require("@whiskeysockets/baileys");
 
-global.sessionName = 'auth-info' 
-const pairingCode = process.argv.includes("--use-pairing-code")
+global.sessionName = "auth-info";
+const pairingCode = process.argv.includes("--use-pairing-code");
 
 if (!pairingCode) {
-  console.log(chalk.redBright("Use --use-pairing-code"))
-  process.exit(1)
+  console.log(chalk.redBright("Use --use-pairing-code"));
+  process.exit(1);
 }
 
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
-const question = (text) => new Promise((resolve) => rl.question(text, resolve))
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const question = (text) => new Promise((resolve) => rl.question(text, resolve));
 
-CFonts.say('Zyy Pairing', {
-      font: 'tiny',
-      align: 'center',
-      colors: ['system']
+CFonts.say("Zyy Pairing", {
+  font: "tiny",
+  align: "center",
+  colors: ["system"],
 });
-CFonts.say('Simple To Connect Whatsapp Bot Use Pairing Code\nWith Baileys Library\n\nGithub : https://github.com/rizzlogy/zyypairing', {
-      colors: ['system'],
-      font: 'console',
-      align: 'center'
-});
+CFonts.say(
+  "Simple To Connect Whatsapp Bot Use Pairing Code\nWith Baileys Library\n\nGithub : https://github.com/rizzlogy/zyypairing",
+  {
+    colors: ["system"],
+    font: "console",
+    align: "center",
+  },
+);
 
 async function main() {
   const sessionExists = await fs.pathExists(path.join(__dirname, sessionName));
@@ -53,47 +59,70 @@ async function ZyyPairing() {
     const socket = makeWASocket({
       printQRInTerminal: !pairingCode,
       logger: pino({
-        level: 'silent'
+        level: "silent",
       }),
-      browser: ['Chrome (Linux)', '', ''], // dont change this.
-      auth: state
+      browser: ["Chrome (Linux)", "", ""], // dont change this.
+      auth: state,
     });
-     if (pairingCode && !socket.authState.creds.registered) {
+    if (pairingCode && !socket.authState.creds.registered) {
       let phoneNumber;
-         phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number : `)))
-         phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
+      phoneNumber = await question(
+        chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number : `)),
+      );
+      phoneNumber = phoneNumber.replace(/[^0-9]/g, "");
 
-         // Ask again when entering the wrong number
-         if (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v))) {
-            console.log(chalk.bgBlack(chalk.redBright("Start with your country's WhatsApp code!")))
-            phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number : `)))
-            phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
-            rl.close()
-         }
+      // Ask again when entering the wrong number
+      if (
+        !Object.keys(PHONENUMBER_MCC).some((v) => phoneNumber.startsWith(v))
+      ) {
+        console.log(
+          chalk.bgBlack(
+            chalk.redBright("Start with your country's WhatsApp code!"),
+          ),
+        );
+        phoneNumber = await question(
+          chalk.bgBlack(
+            chalk.greenBright(`Please type your WhatsApp number : `),
+          ),
+        );
+        phoneNumber = phoneNumber.replace(/[^0-9]/g, "");
+        rl.close();
+      }
 
       setTimeout(async () => {
-         let code = await socket.requestPairingCode(phoneNumber)
-         code = code?.match(/.{1,4}/g)?.join("-") || code
-         console.log(chalk.black(chalk.bgGreen(`Your Pairing Code : `)), chalk.black(chalk.white(code)))
-      }, 3000)
-     }
-    socket.ev.on('connection.update', async ({ connection, lastDisconnect }) => {
-      if (connection === 'open') {
-          console.log(chalk.greenBright('DONE!'));
+        let code = await socket.requestPairingCode(phoneNumber);
+        code = code?.match(/.{1,4}/g)?.join("-") || code;
+        console.log(
+          chalk.black(chalk.bgGreen(`Your Pairing Code : `)),
+          chalk.black(chalk.white(code)),
+        );
+      }, 3000);
+    }
+    socket.ev.on(
+      "connection.update",
+      async ({ connection, lastDisconnect }) => {
+        if (connection === "open") {
+          console.log(chalk.greenBright("DONE!"));
           await fs.emptyDir("./" + sessionName);
           process.exit(1);
-      } else if (connection === 'close' && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode && lastDisconnect.error.output.statusCode !== 401) {
-        ZyyPairing();
-         await fs.emptyDir("./" + sessionName);
-  } 
-})
-      socket.ev.on('creds.update', saveCreds);
-
+        } else if (
+          connection === "close" &&
+          lastDisconnect &&
+          lastDisconnect.error &&
+          lastDisconnect.error.output.statusCode &&
+          lastDisconnect.error.output.statusCode !== 401
+        ) {
+          ZyyPairing();
+          await fs.emptyDir("./" + sessionName);
+        }
+      },
+    );
+    socket.ev.on("creds.update", saveCreds);
   } catch (error) {
     console.error(error);
-     await fs.emptyDir("./" + sessionName);
+    await fs.emptyDir("./" + sessionName);
     process.exit(1);
   }
 }
 
-main().catch(error => console.error(error));
+main().catch((error) => console.error(error));
